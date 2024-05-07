1aece485cf53db3e0890d8c05304c6047af9d600
Lucas Exequiel Vargas <lucasvargasexequiel@gmail.com>
2024-05-07 20:01:19 -0300

    Avances (#2)

--- Etapa 2/TAD_TCad.c
+++ Etapa 2/TAD_TCad.c
@@ -1,11 +1,11 @@
-#include "TAD_Cad.h"
+#include "TAD_TCad.h"
 #include <stdio.h>
 
 void leeCad(char cad[],int tam){
 	int j=0;
 	char c;
 	c=getchar();
-	while(c!=EOF && c!= '\n' && j < n-1){
+	while(c!=EOF && c!= '\n' && j < tam-1){
 		cad[j]=c;
 		c=getchar();
 		j++;
